2ed5d2d9c3fbb8d464c06b6975c4502096d992a1
gliger13 <zanevskiyandrey@gmail.com>
2021-02-25 17:56:08 +0300

    Refactor: remove unused code

--- test_api/conftest.py
+++ test_api/conftest.py
@@ -1,14 +1,4 @@
 import pytest
-import requests
-
-
-@pytest.fixture(params=[{
-    'keeper_url': '0.0.0.0:8002',
-    'reaper_url': '0.0.0.0:8001',
-    'master_url': '0.0.0.0:8000',
-}])
-def api_url():
-    return 'http://0.0.0.0:8002'
 
 
 @pytest.fixture
